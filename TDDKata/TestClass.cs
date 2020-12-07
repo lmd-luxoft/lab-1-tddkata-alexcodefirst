a6377115c862d07686cf2ee1e3af590c1a918983
alexcodefirst <a.boychenko.nn@mail.ru>
2020-12-07 15:14:01 +0300

    add test scenarios

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -1,7 +1,5 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace TDDKata
@@ -9,12 +7,89 @@ namespace TDDKata
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void SimpleTest()
+        private StringCalc _stringCalc;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
         {
-            StringCalc calc = new StringCalc();
-            int value = calc.Sum("2,2");
-            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+            _stringCalc = new StringCalc();
         }
+
+        [Test]
+        public void Should_ReturnSum_When_ArgumentsHaveTwoPositiveNumbers()
+        {
+            string arguments = "1,1";
+            int expected = 2;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_ReturnNumber_When_ArgumentsHaveOnlyOnePositiveNumber()
+        {
+            string arguments = "1";
+            int expected = 1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase("0")]
+        public void Should_ReturnZero_When_ArgumementsAreWhiteSpaceOrZero(string arguments)
+        {
+            int expected = 0;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_ThrowException_When_ArgumementsAreNull()
+        {
+            string arguments = null;
+            int expected = -1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase("1,1,2")]
+        [TestCase("1,1,2,3")]
+        public void Should_ThrowException_When_ArgumentsHaveMoreThanTwoNumbers(string arguments)
+        {
+            int expected = -1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,-1")]
+        [TestCase("-1,1")]
+        [TestCase("-1,-1")]
+        public void Should_ThrowException_When_ArgumentsHaveNegativeNumbers(string arguments)
+        {
+            int expected = -1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("qwerty")]
+        [TestCase("1,qw")]
+        [TestCase("az,1")]
+        public void Should_ThrowException_When_ArgumentsHaveNoNumbers(string arguments)
+        {
+            int expected = -1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1;1")]
+        [TestCase("1.1")]
+        public void Should_ThrowException_When_ArgumentsSeparatotIsIncorrect(string arguments)
+        {
+            int expected = -1;
+            int actual = _stringCalc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
